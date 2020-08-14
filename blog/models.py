68a71b6cd39aa02a7b90cf5ccb0fb5d807ffad66
Saif <saiofa99@gmail.com>
2020-08-14 14:29:34 +0200

    can add images to blogs

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,7 @@ class Blog(models.Model):
 	upload_date = models.DateTimeField(default=timezone.now)
 	# author = models.ForeignKey(User, on_delete=models.CASCADE)
 	author = models.ForeignKey(Author, on_delete=models.CASCADE)
+	image_src = models.CharField(max_length=200, blank=True, null=True)
 
 	def __str__(self):
 		return self.title
